126fc708125930e5dcd08922f1ae72dd1d64ed81
Bahar Pourazar <pourazar.bahare@gmail.com>
2013-12-05 12:44:43 -0330

    dynamic filename passed as input to dilate call

--- disparity/dilate.cpp
+++ disparity/dilate.cpp
@@ -11,21 +11,23 @@ int main (int argc, char* argv[]){
 
 	int itr;
 	Mat src;
-	if(argc<3){
-		cerr << "Usage: ./dilate image iter";
+	if(argc<4){
+		cerr << "Usage: ./dilate image iter imgName" << endl;
 		return -1;
 	}
 	src = imread(argv[1] , 0);
 	itr = atoi(argv[2]);
-	cerr << "itr: " << itr << endl;
+	stringstream ss;
+	ss << argv[3] << ".png" ;
+	string fname = ss.str();
+	cerr << "fname: " <<  fname <<  " itr: " << itr << endl;
 
-	Mat dst1,dst2;
+	Mat dst1;
 	dilate(src, dst1, Mat(), Point(-1,-1),itr);
-	erode(dst1, dst2, Mat(), Point(-1,-1),itr);
 
 	imshow( "dilate" , dst1);
-	imshow( "erode" , dst2);
-	imwrite( "/home/bahar/Dataset/gtfilled/gt.png" , dst2);
+	string fpath = "/home/bahar/Dataset/gtfilled/" + fname;
+	imwrite( fpath , dst1);
 	waitKey(0);
 	return 0;
 }
