ec5c8dc9701ccae4ed79803b5a18bddcffa46158
Bahar Pourazar <pourazar.bahare@gmail.com>
2013-12-05 17:00:13 -0330

    changes applied to canny to get also filename and number of iteration to expand edges for mask generation

--- disparity/canny.cpp
+++ disparity/canny.cpp
@@ -7,7 +7,7 @@
 using namespace cv;
 using namespace std;
 
-Mat src, src_gray;
+Mat src, org_src, src_gray;
 Mat dst, detected_edges;
 
 int edgeThresh = 1;
@@ -16,7 +16,7 @@ int const max_lowThreshold = 100;
 int ratio;
 int kernel_size = 3;
 char* window_name = (char*) "Edge Map";
-
+int itr;
 /**
    * @function CannyThreshold
     * @brief Trackbar callback - Canny thresholds input with a ratio 1:3
@@ -25,15 +25,14 @@ void CannyThreshold(int, void*)
 {
 	/// Reduce noise with a kernel 3x3
 	blur( src_gray, detected_edges, Size(3,3) );
-	  
 	/// Canny detector
 	Canny( detected_edges, detected_edges, lowThreshold, lowThreshold*ratio, kernel_size );
 	    
 	/// Using Canny's output as a mask, we display our result
 	dst = Scalar::all(0);
 	      
-	dilate(detected_edges,detected_edges, Mat(), Point(-1,-1), 2);
-	src.copyTo( dst, detected_edges);	//Mask the src based on the detected edges
+	dilate(detected_edges,detected_edges, Mat(), Point(-1,-1), itr);
+	org_src.copyTo( dst, detected_edges);	//Mask the src based on the detected edges
 //	dst.convertTo(dst, -1,3,0);		
 	imshow( window_name, dst );
 	imshow( "detected_edges mask", detected_edges );
@@ -41,17 +40,25 @@ void CannyThreshold(int, void*)
 
 int main(int argc, char* argv[]){
 	
-	if(argc != 3){
-		cout << "Usage: ./canny image ratio " << endl;
+	if(argc != 6){
+		cout << "Usage: ./canny smoothed_gt original_gt ratio dilate_itr filename" << endl;
 		return -1;
 	}
 	/// Load an image
-	src = imread( argv[1],0 );
-	ratio = atoi(argv[2]); 
-	if( !src.data ) { return -1; }
+	src = imread( argv[1],-1);
+	org_src = imread(argv[2], -1);
+
+	cout << "src: " << src.type() << " org_src: " << src.type() << endl;
+	ratio = atoi(argv[3]); 
+	itr = atoi(argv[4]); 
+	stringstream ss;
+	ss << argv[5] << ".png";
+	string fname = ss.str();
+	cout << "fname: " << fname << endl;
+	if( !src.data || !org_src.data ) { return -1; }
 
 	/// Create a matrix of the same type and size as src (for dst)
-	dst.create( src.size(), src.type() );
+	dst.create( org_src.size(), org_src.type() );
 
 	/// Convert the image to grayscale
 	if(src.depth()!=CV_8U){
@@ -71,8 +78,9 @@ int main(int argc, char* argv[]){
 
 	/// Wait until user exit program by pressing a key
 	waitKey(0);
-
-	imwrite("/home/bahar/Dataset/masks/mask.png" , detected_edges);
-	imwrite("/home/bahar/Dataset/maskedgt/maskgt.png" , dst);
+	string fpath1 = "/home/bahar/Dataset/masks/" + fname;
+	string fpath2 = "/home/bahar/Dataset/maskedgt/" + fname;
+	imwrite(fpath1, detected_edges);
+	imwrite(fpath2 , dst);
 	return 0;
 }
