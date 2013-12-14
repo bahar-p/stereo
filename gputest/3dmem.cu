d0fd62187edb3294929ac62a662a177dda7cb383
Bahar Pourazar <pourazar.bahare@gmail.com>
2013-12-14 14:48:05 -0330

    gpu3mem test sample is done now, kernel implemented and works

--- gputest/3dmem.cu
+++ gputest/3dmem.cu
@@ -8,7 +8,6 @@
 #include "cuda_runtime.h"
 #include <opencv2/gpu/gpu.hpp>
 
-
 using namespace std;
 using namespace cv;
 using namespace cv::gpu;
@@ -17,94 +16,79 @@ int drange = 2;
 int width = 10;
 int height = 10;
 cv::Size s = cv::Size(height,width);
+int N = width*height;
+
+static __global__ void mykernel(cv::gpu::PtrStepSz<float> src, cv::gpu::PtrStep<float> dst){
+	int x = threadIdx.x + blockIdx.x * blockDim.x;
+	int y = threadIdx.y + blockIdx.y * blockDim.y;
+	if( x>= src.cols || y >= src.rows) return;
+
+	dst(y,x) = src(y,x) + 2.0;
+//	float a = src(y,x) + 2.0;
+//	dst.ptr(y)[x] = a;
+	return;
+}
+
+int d_caller (const cv::gpu::PtrStepSz<float>& src, const cv::gpu::PtrStepSz<float>& dst) {
+
+	unsigned int block_dimX = (unsigned int)std::ceil(src.cols/(float)8);
+	unsigned int block_dimY = (unsigned int)std::ceil(src.rows/(float)8);
+	dim3 blocks(block_dimX,block_dimY);
+	dim3 threads(8,8);
+	mykernel<<<blocks, threads>>>(src, dst);
+	cudaDeviceSynchronize();
+//	}
+		//check for error
+		cudaError_t err;
+		err = cudaGetLastError();
+		if(err != cudaSuccess){
+			std::cout << "mykernel: CUDA error with kernel: " << cudaGetErrorString(err) << std::endl;
+			return 1;
+		}
+		return 0;
+}
 
 int main(int argc, char **argv)
 {
-	cudaError_t result;
-	//size_t mysize = width * height * drange * sizeof(float);
-	//cv::Size size = cv::Size(height, width);
+	//cudaError_t result;
 	cv::Mat* h_in = new cv::Mat[drange];
+	cv::Mat* h_out = new cv::Mat[drange];
 	cv::gpu::GpuMat* h_gpin = new cv::gpu::GpuMat[drange];
-	cv::gpu::GpuMat* d_gpin = new cv::gpu::GpuMat[drange];
-	cv::gpu::GpuMat* d_out = new cv::gpu::GpuMat[drange];
+	cv::gpu::GpuMat* d_gpout = new cv::gpu::GpuMat[drange];
 
-//	h_in = (struct mtype*) malloc(drange*sizeof(struct mtype)); 
 	for(int i =0 ; i<drange ; i++){
 		h_in[i] = cv::Mat(height,width, CV_32F, cv::Scalar::all(1.0));
+		h_out[i] = cv::Mat(height,width, CV_32F);
 		h_gpin[i] = cv::gpu::GpuMat(height, width, CV_32F);
-		d_gpin[i] = cv::gpu::GpuMat(height, width, CV_32F);
-		d_out[i] = cv::gpu::GpuMat(height, width, CV_32F);
-		//cv::gpu::GpuMat plane;
-		//plane.upload(h_in[i]);
+		createContinuous(height, width, CV_32F, d_gpout[i]);
 		h_gpin[i].upload(h_in[i]);
 	}
 
-	result = cudaMalloc((void**)&d_gpin,drange*width*height*sizeof(float));
-
+/*	result = cudaMalloc((void**)&d_gpin,drange*width*height*sizeof(float));
+	result = cudaMalloc((void**)&d_gpout,drange*width*height*sizeof(float));
 	if (result != cudaSuccess) {
 		fprintf(stderr, "Failed to allocate array of GpuMat on device - %s\n", cudaGetErrorString(result));
 		return 1;
 	}
-	
+
 	result = cudaMemcpy(d_gpin, h_gpin, drange*sizeof(cv::Mat), cudaMemcpyHostToDevice);
 
 	if (result != cudaSuccess) {
 		fprintf(stderr, "Failed to copy array of gpumats from host to device- %s\n", cudaGetErrorString(result));
 		return 1;
 	}
+*/
 
-
-	result = cudaMemcpy(d_out, d_gpin, drange*sizeof(cv::Mat), cudaMemcpyDeviceToHost);
-	cv::Mat h_out;
-	d_out[0].download(h_out);
-	std::cout << "test: " << h_out << std::endl;
-
-/*
-	//surface<void, cudaSurfaceType3D> surfRef;
-	result = cudaBindSurfaceToArray(surfRef, arrayPointer);
-	if (result != cudaSuccess) {
-		fprintf(stderr, "cudaBindTextureToArray() - failed to bind texture to array - %s\n", cudaGetErrorString(result));
-		return 2;
-	}
-
-	const dim3 dimBlock(6,6,6);
-	const dim3 dimGrid((width + dimBlock.x-1)/ dimBlock.x, (height + dimBlock.y-1)/dimBlock.y, (depth + dimBlock.z-1 )/dimBlock.z);
-
-	costAD<<<dimGrid,dimBlock>>>(d_left, d_right,surfRef, minDisp, maxDisp, subRW, subRH);
-
-	cudaMemcpy3DParms params = {0};
-	memset(&params, 0, sizeof(params));
-	params.dstPtr.pitch = sizeof(float) * width;
-	params.dstPtr.ptr = h_out;
-	params.dstPtr.xsize = width;
-	params.dstPtr.ysize = height;
-	params.srcPos.x = 0;
-	params.srcPos.y = 0;
-	params.srcPos.z = 0;
-	params.srcArray = arrayPointer;
-	params.dstPos.x = 0;
-	params.dstPos.y = 0;
-	params.dstPos.z = 0;
-	params.extent.width = width;
-	params.extent.depth = depth;
-	params.extent.height = height;
-	params.kind = cudaMemcpyDeviceToHost;
-	result = cudaMemcpy3D(&params);
-	if (result != cudaSuccess) {
-		fprintf(stderr, "cudaMemcpy3D - failed to copy from array to host buffer for verification - %s\n", cudaGetErrorString(result));
-		return 1;
-	}
-
-	cudaThreadSynchronize();
-	for (int k = 0; k <depth; k++) {
-		for (int j = 0; j < height; j++) {
-			for (int i = 0; i < width; i++) {
-				printf(" gpuh_DSIResulft: %f \n", h_out[i + j * width+ k*width*height]);
-				h_DSI.at<double>(i,j,k) = (double) h_out[i + j * width + k * width * height];
-			}
+	for(int i=0; i<drange; i++){
+		if (d_caller(h_gpin[i] , d_gpout[i])!=0) {
+			std::cout << "kernel call unsuccessful! " << std::endl;
+			return -1;
 		}
+		d_gpout[i].download(h_out[i]);
+		std::cout << "hout["<< i << "]= " << h_out[i] << std::endl;
 	}
-	cudaFreeArray(arrayPointer);*/
+	//result = cudaMemcpy(h_gpout, d_gpout, drange*sizeof(cv::Mat), cudaMemcpyDeviceToHost);
+
 	return 0;
 }
 
