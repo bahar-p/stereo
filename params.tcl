a3f91ba6a89d4f86f6bb5275561f7e4f892d95eb
Bahar Pourazar <pourazar.bahare@gmail.com>
2013-12-19 17:55:45 -0330

    new script containing shared parameters between other scripts

---
+++ params.tcl
@@ -0,0 +1,7 @@
+#!/usr/bin/env tclsh
+
+set ::mainp "/home/bahar/Master/stereo/"
+set ::adcensus "build/main"
+set ::adcensus "${mainp}${adcensus}"
+set ::sgbm "build/kitdisp"
+set ::sgbm "${mainp}${sgbm}"
