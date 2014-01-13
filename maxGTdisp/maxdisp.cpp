a1d90e52798860341af89bd7e947b90d4df53ffd
Bahar Pourazar <pourazar.bahare@gmail.com>
2014-01-13 12:38:51 -0330

    new function to find maxdisp value in GTs implemented

---
+++ maxGTdisp/maxdisp.cpp
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <fstream>
+#include <string.h>
+#include "cv.h"
+#include "highgui.h"
+
+int main (int argc, char** argv){
+	if(argc < 2) {
+		std::cerr << "Usage: ./maxdisp gt_disp" << std::endl;
+		return -1;
+	}
+	cv::Mat im_gt = cv::imread(argv[1],-1);
+	cv::Mat gt;
+	im_gt.convertTo(gt, CV_32F, 1/256.);
+	char* fullpath = argv[1];
+	char* bname = basename(fullpath);
+	std::string fname = (reinterpret_cast<char*>(bname));
+
+	float max = im_gt.at<float>(0,0);
+	for(int j=0; j< gt.rows; j++){
+		for(int i=0;i<gt.cols; i++){
+			float d_gt = gt.at<float>(j,i);
+			if( d_gt >= max){
+				max = d_gt;
+			}
+		}
+	}
+	std::cout << "gonna write to file, value: " << max << std::endl;
+	std::ofstream myfile;
+	myfile.open("/home/bahar/Master/stereo/maxGTdisp/maxdisp.txt", std::ios::out | std::ios::app);
+	myfile << "image: " << fname << " maxDisp: " << max << std::endl;
+	return 0;
+}
