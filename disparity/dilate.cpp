fb39cfbe5ebc702b9623eab101b5d446490efd9e
Bahar Pourazar <pourazar.bahare@gmail.com>
2014-01-14 13:17:13 -0330

    dilate code now accepts a noc flag to save the result in the correct directory and also is handled with a track bar now and also saves selected itr in a txt fike

--- disparity/dilate.cpp
+++ disparity/dilate.cpp
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <iostream>
+#include <fstream>
+#include <string.h>
 #include <stdlib.h>
 #include "cv.h"
 #include "highgui.h"
@@ -7,28 +9,48 @@
 using namespace std;
 using namespace cv;
 
-int main (int argc, char* argv[]){
+Mat src, dst;
+int itr;
+const int max_itr = 10;
+char* window_name = "";
 
-	int itr;
-	Mat src;
-	if(argc !=3){
-		cerr << "Usage: ./dilate image iter" << endl;
+void DilateIter(int, void*){
+	dilate(src, dst, Mat(), Point(-1,-1),itr);
+	imshow( window_name, dst);
+}
+
+int main (int argc, char* argv[]){
+	if(argc < 3 ){
+		cerr << "Usage: ./dilate image is_noc ?iter?" << endl;
 		return -1;
 	}
 	src = imread(argv[1] , 0);
-	itr = atoi(argv[2]);
+	int noc = atoi(argv[2]);
+	if(argc==4) itr = atoi(argv[3]);
+	else itr=0;
 	char* fullname = argv[1];
 	char* bname = basename(fullname);
 	string fname = reinterpret_cast<char*>(bname);
-	//cerr << "fname: " <<  fname << endl;
-
-	Mat dst1;
-	dilate(src, dst1, Mat(), Point(-1,-1),itr);
-
-	//imshow( "dilate" , dst1);
-	string fpath = "/home/bahar/Master/stereo/gtfill/" + fname;
-	imwrite( fpath , dst1);
-	//waitKey(0);
+	
+	//Create Window to display
+	window_name = (char *) fname.c_str();
+	namedWindow(window_name, CV_WINDOW_AUTOSIZE);
+	//Create Trackbar
+	createTrackbar("Dilate iteration:" ,window_name, &itr, max_itr, DilateIter);
+	//Callback function
+	DilateIter(itr,0);
+	waitKey(0);
+	string fpath;
+	ofstream myfile;
+	if(noc){
+		myfile.open("/home/bahar/Master/stereo/gtfill/dilItr.txt", std::ios::out | std::ios::app);
+		fpath = "/home/bahar/Master/stereo/gtfill/disp_noc/" + fname;
+	}
+	else{
+		fpath = "/home/bahar/Master/stereo/gtfill/disp_occ/" + fname;
+	}
+	myfile << "img: " << fname << " dil_itr: " << itr << endl;
+	imwrite( fpath , dst);
 	return 0;
 }
 
