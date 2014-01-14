05a42feaee73158d9b2b91c7ff28548f50dd0c26
Bahar Pourazar <pourazar.bahare@gmail.com>
2014-01-14 15:08:19 -0330

    occ and noc call seperated

--- scripts/mydilate.tcl
+++ scripts/mydilate.tcl
@@ -4,8 +4,14 @@ source ../params.tcl
 
 proc dilate {img is_noc {itr 0}} {
 	set nu $img
-	if { [catch {exec $::dilate "${::gt_noc}${nu}" $is_noc $itr} msg] } {
-		puts "Error: $::errorInfo and $msg"
+	if {$is_noc} {
+		if { [catch {exec $::dilate "${::gt_noc}${nu}" $is_noc $itr} msg] } {
+			puts "Error: $::errorInfo and $msg"
+		}
+	} else {
+		if { [catch {exec $::dilate "${::gt_occ}${nu}" $is_noc $itr} msg] } {
+			puts "Error: $::errorInfo and $msg"
+		}
 	}
 }
 
