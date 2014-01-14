895ca7babb33e6a1cfada03c2481c3324178aeb2
Bahar Pourazar <pourazar.bahare@gmail.com>
2014-01-14 15:09:29 -0330

    masking scripts modified to get noc flag and make calls to mask function

--- scripts/callmask.tcl
+++ scripts/callmask.tcl
@@ -2,21 +2,12 @@
 
 source ./mymask.tcl
 
-proc runmask { } {
-	mask [list 000005.png 7]
-	mask [list 000009.png 4]
-	mask [list 000016.png 5]
-	mask [list 000037.png 5]
-	mask [list 000039.png 4]
-	mask [list 000060.png 4]
-	mask [list 000064.png 7]
-	mask [list 000081.png 5]
-	mask [list 000086.png 4]
-	mask [list 000110.png 5]
-	mask [list 000116.png 5]
-	mask [list 000133.png 5]
-	mask [list 000138.png 8]
-	mask [list 000169.png 15]
+proc run_mask {myargs} {
+	set noc [lindex $myargs 0]	
+	set imglist [lrange $myargs 1 end]
+	foreach img $imglist {
+		mask $img $noc
+	}
 }
 
-runmask
+run_mask $::argv
--- scripts/mymask.tcl
+++ scripts/mymask.tcl
@@ -3,11 +3,16 @@ source ../params.tcl
 set d_itr 10
 set ratio 3
 
-proc mask {myargs} {
+proc mask {img is_noc {min_thresh 0}} {
 	global d_itr ratio
-	set nu [lindex $myargs 0]
-	set min_thresh [lindex $myargs 1]
-	if { [catch {exec $::canny "${::gtf_noc}${nu}" "${::gt_noc}${nu}" $ratio $d_itr $min_thresh} msg ] } {
-		puts "ERROR: $::errorInfo and $msg"
+	set nu $img
+	if {$is_noc} {
+		if { [catch {exec $::canny "${::gtf_noc}${nu}" "${::gt_noc}${nu}" $ratio $d_itr $is_noc $min_thresh} msg ] } {
+			puts "ERROR: $::errorInfo and $msg"
+		}
+	} else {
+		if { [catch {exec $::canny "${::gtf_occ}${nu}" "${::gt_occ}${nu}" $ratio $d_itr $is_noc $min_thresh} msg ] } {
+			puts "ERROR: $::errorInfo and $msg"
+		}
 	}
 }
