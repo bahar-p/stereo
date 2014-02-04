68b6ccf0376643f35ce3f671ce89589a3205bfc0
Bahar Pourazar <pourazar.bahare@gmail.com>
2014-02-04 00:05:48 -0330

    mask flag added so results can be evaluated both for masked on un-masked cases

--- scripts/myeval.tcl
+++ scripts/myeval.tcl
@@ -9,29 +9,59 @@ proc evl {myargs} {
 	set adc [lindex $myargs 3]
 	set f [lindex $myargs 4]
 	set bs [lindex $myargs 5]
-	
+	set msk [lindex $myargs 6]	
+	puts "img: $nu"
 	if {$noc} {
 		if {$adc} {
-			if { [catch { exec $::evals "${::gtm_noc}${nu}" "${::adc_ms_noc}${nu}" $maxd $f $bs $adc $noc "${::mask_noc}${nu}"} msg] } {
-				puts "Something seems to have gone wrong:"
-				puts "Information about it: $::errorInfo and $msg"
+			if {$msk} {	
+				if { [catch { exec $::evals "${::gtm_noc}${nu}" "${::adc_ms_noc}${nu}" $maxd $f $bs $adc $noc "${::mask_noc}${nu}"} msg] } {
+					puts "Something seems to have gone wrong:"
+					puts "Information about it: $::errorInfo and $msg"
+				}
+			} else {
+				if { [catch { exec $::evals "${::gt_noc}${nu}" "${::adc_noc}${nu}" $maxd $f $bs $adc $noc} msg] } {
+					puts "Something seems to have gone wrong:"
+					puts "Information about it: $::errorInfo and $msg"
+				}
 			}
 		} else {
-			if { [catch { exec $::evals "${::gtm_noc}${nu}" "${::sg_ms_noc}${nu}" $maxd $f $bs $adc $noc "${::mask_noc}${nu}"} msg] } {
-				puts "Something seems to have gone wrong:"
-				puts "Information about it: $::errorInfo and $msg"
+			if {$msk} {
+				if { [catch { exec $::evals "${::gtm_noc}${nu}" "${::sg_ms_noc}${nu}" $maxd $f $bs $adc $noc "${::mask_noc}${nu}"} msg] } {
+					puts "Something seems to have gone wrong:"
+					puts "Information about it: $::errorInfo and $msg"
+				}
+			} else {
+				#puts "${::gt_noc}${nu} ${::sg_noc}${nu} $maxd $f $bs $adc $noc"
+				if { [catch { exec $::evals "${::gt_noc}${nu}" "${::sg_noc}${nu}" $maxd $f $bs $adc $noc} msg] } {
+					puts "Something seems to have gone wrong:"
+					puts "Information about it: $::errorInfo and $msg"
+				}
 			}
-		} 
+		}
 	} else {
 		if {$adc} {
-			if { [catch { exec $::evals "${::gtm_occ}${nu}" "${::adc_ms_occ}${nu}" $maxd $f $bs $adc $noc "${::mask_occ}${nu}"} msg] } {
-				puts "Something seems to have gone wrong:"
-				puts "Information about it: $::errorInfo and $msg"
-			} 
+			if {$msk} {
+				if { [catch { exec $::evals "${::gtm_occ}${nu}" "${::adc_ms_occ}${nu}" $maxd $f $bs $adc $noc "${::mask_occ}${nu}"} msg] } {
+					puts "Something seems to have gone wrong:"
+					puts "Information about it: $::errorInfo and $msg"
+				}
+			} else {
+				if { [catch { exec $::evals "${::gt_occ}${nu}" "${::adc_occ}${nu}" $maxd $f $bs $adc $noc} msg] } {
+					puts "Something seems to have gone wrong:"
+					puts "Information about it: $::errorInfo and $msg"
+				}
+			}
 		} else {
-			if { [catch { exec $::evals "${::gtm_occ}${nu}" "${::sg_ms_occ}${nu}" $maxd $f $bs $adc $noc "${::mask_occ}${nu}"} msg] } {
-				puts "Something seems to have gone wrong:"
-				puts "Information about it: $::errorInfo and $msg"
+			if {$msk} {
+				if { [catch { exec $::evals "${::gtm_occ}${nu}" "${::sg_ms_occ}${nu}" $maxd $f $bs $adc $noc} msg] } {
+					puts "Something seems to have gone wrong:"
+					puts "Information about it: $::errorInfo and $msg"
+				}
+			} else {
+				if { [catch { exec $::evals "${::gt_occ}${nu}" "${::sg_occ}${nu}" $maxd $f $bs $adc $noc "${::mask_occ}${nu}"} msg] } {
+					puts "Something seems to have gone wrong:"
+					puts "Information about it: $::errorInfo and $msg"
+				}
 			}
 		}
 	}
