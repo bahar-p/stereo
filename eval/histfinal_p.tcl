3b1bf43ef37a05be45a64ae8abc668a22d8138a7
Bahar Pourazar <pourazar.bahare@gmail.com>
2014-02-27 19:44:55 -0330

    plotting updated and binocularvision chapter updated

--- eval/histfinal_p.tcl
+++ eval/histfinal_p.tcl
@@ -8,14 +8,19 @@ proc finalplot {f} {
 		set term x11
 		set xlabel \"Actual Depth(m)\"
 		set ylabel \"Stereo Acuity(arcmin)\"
-		plot \[:\]\[0:2\] \"$f\" every ::1 using 1:2 with points notitle, \
-			\"$f\" every ::1 using 1:3 with lines notitle, \
-			\"$f\" every ::1 using 1:4 with lines notitle, \
-			\"$f\" every ::1 using 1:5 with lines notitle
+		set title \"Average Stereoacuity over different depth ranges\"
+		set key top right
+		plot \[:\]\[0:2\] \"$f\" every ::1 using 1:2 with points t \"Avg Error\", \
+			\"$f\" every ::1 using 1:3 with lines t \"Age:17-29\", \
+			\"$f\" every ::1 using 1:4 with lines t \"Age:30-49\", \
+			\"$f\" every ::1 using 1:5 with lines t \"Age:50-69\"
 		MIN=GPVAL_DATA_Y_MIN
 		MAX=GPVAL_DATA_Y_MAX
 		set term png
 		set output \"${::plots}finalhist.png\"
+		replot
+		set term postscript eps enhanced colour dashed lw 1 \"Helvetica\" 14
+		set output \"${::plots}test.eps\" 
 		replot
 		reset
 		set term x11
