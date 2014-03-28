776f3b2b56abeec0216cec2db74f4ec824160f2d
Bahar Pourazar <pourazar.bahare@gmail.com>
2014-03-28 21:23:19 -0230

    line to check for argc first added

--- eval/readimg.cpp
+++ eval/readimg.cpp
@@ -5,6 +5,10 @@
 #include <fstream>
 
 int main(int argc, char** argv){
+	if(argc<4) {
+		std::cout << " Usage: ./readimage disp dmax Isadc" << std::endl;
+		return -1;
+	}
 	cv::Mat disp = cv::imread(argv[1],-1);
 	int dmax = atoi(argv[2]);
 	int adc = atoi(argv[3]);
