9473c4e78bc8f67647217d2ae218bf32ca68a39c
Bahar Pourazar <pourazar.bahare@gmail.com>
2014-04-08 16:49:04 -0230

    scripts to call the code for stacuity threshold detection by algorithm added

---
+++ scripts/mydiscr.tcl
@@ -0,0 +1,27 @@
+#!/usr/bin/env tclsh
+source ../params.tcl
+
+proc disc {myargs} {
+
+	set nu [lindex $myargs 0]
+	set maxd [lindex $myargs 1]
+	set pixthr [lindex $myargs 2]
+	set f [lindex $myargs 3]
+	set b [lindex $myargs 4]
+	set w [lindex $myargs 5]
+	set h [lindex $myargs 6]
+	set adc [lindex $myargs 7]
+	puts "img: $nu"
+		if {$adc} {
+			if { [catch { exec $::grad "${::gt_noc}${nu}" "${::adc_noc}${nu}" $maxd $pixthr $f $b $w $h $adc} msg] } {
+				puts "Something seems to have gone wrong:"
+				puts "Information about it: $::errorInfo and $msg"
+			}
+		} else {
+			if { [catch { exec $::grad "${::gt_noc}${nu}" "${::sg_noc}${nu}" $maxd $pixthr $f $b $w $h $adc} msg] } {
+				puts "Something seems to have gone wrong:"
+				puts "Information about it: $::errorInfo and $msg"
+			}
+		}
+}
+
